55b0bb10dbcd72f84752b81fa8255f150ffbddea
ale10183 <alexandre.rodrigues31@escola.pr.gov.br>
2026-08-18 09:49:27 -0300

    meu projeto

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ botoes.forEach(botao => {
     botao.addEventListener('click', curtir)
     let curtiu = false 
     function curtir() {
-        let curtidas = document.querySelector('span')
+        let curtidas = botao.querySelector('span')
         
        if(curtiu == false){
            curtiu = true
            curtidas.textContent++
            botao.style.backgroundColor = 'pink'
+       }else{
+          curtiu = false
+          curtidas.textContent--
+          botao.style.backgroundColoe = 'lightgr'
        }
         
     }
